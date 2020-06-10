40492a13345a98aa94d497c85b9ae6b11fbebcec
Peter Lohse <peter.lohse@fsd-web.de>
2020-06-10 19:05:35 +0200

    license and readme added

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@
 del files.tar
 7z a -ttar -mx=9 files.tar .\files\*
 del dev.hanashi.wsc.twemoji-smileys.tar
-7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-smileys.tar .\* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.twemoji-smileys.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-smileys.tar .\* -x!acptemplates -x!README.adoc -x!files -x!templates -x!dev.hanashi.wsc.twemoji-smileys.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ rm -f files.tar
 7z a -ttar -mx=9 files.tar ./files/*
 rm -f dev.hanashi.wsc.twemoji-smileys.tar
 rm -f dev.hanashi.wsc.twemoji-smileys.tar.gz
-7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-smileys.tar ./* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.twemoji-smileys.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-smileys.tar ./* -x!acptemplates -x!files -x!README.adoc -x!templates -x!dev.hanashi.wsc.twemoji-smileys.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
 7z a dev.hanashi.wsc.twemoji-smileys.tar.gz dev.hanashi.wsc.twemoji-smileys.tar
 rm -f files.tar
 rm -f dev.hanashi.wsc.twemoji-smileys.tar
